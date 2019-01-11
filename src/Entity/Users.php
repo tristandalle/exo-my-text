e3db2213ba5337f1647dddd6c6d27df8dfeb1bcd
tristandalle <tristandalle@gmail.com>
2019-01-11 11:07:54 +0100

    create entity

---
+++ src/Entity/Users.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ */
+class Users
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pseudo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_mail;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): self
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getUserMail(): ?string
+    {
+        return $this->user_mail;
+    }
+
+    public function setUserMail(string $user_mail): self
+    {
+        $this->user_mail = $user_mail;
+
+        return $this;
+    }
+}
